6200c4436f9a25894899289bcd88d3b135e8e105
Daniil <80905799+DaniilGavrin@users.noreply.github.com>
2025-07-19 03:58:51 +0300

    Настройки отключены

--- lib/screens/events_screen.dart
+++ lib/screens/events_screen.dart
@@ -34,10 +34,12 @@ class _EventsScreenState extends State<EventsScreen> {
             ),
             onPressed: () => context.read<ThemeProvider>().toggleTheme(),
           ),
+          /*
           IconButton(
             icon: const Icon(Icons.settings),
             onPressed: () => context.push('/settings'),
           ),
+          */
           IconButton(
             icon: const Icon(Icons.add),
             onPressed: _showAddEventDialog,
